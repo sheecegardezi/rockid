0099459bb34cc35e4d7c0deeb80d1f572e10c9d6
sheece <sheeced@sheeces-MacBook-Pro.local>
2017-11-03 19:25:50 +1100

    update script

--- main.py
+++ main.py
@@ -1,59 +1,87 @@
 import os
 import cv2
 import numpy as np
-from utils import *
 
-drawing=False
+
 currentframe='currentframe.jpg'
 windowName='window'
-
-def draw(event, x, y, flags, param):
-    global ix, iy, drawing
-    ix, iy = x, y
-
-    if event == cv2.EVENT_RBUTTONDOWN and not drawing:
-        drawing = True
-
-    if event == cv2.EVENT_LBUTTONDOWN:
-        print('')
-
-
+line1=[]
+root= 'data/processed'
 cv2.namedWindow(windowName)
-cv2.setMouseCallback(windowName,draw)
 
-root= 'data'
-
-listOfFiles= [f for f in os.listdir(root) if os.path.isfile(os.path.join(root, f)) and f.__contains__('.JPG')]
-print(listOfFiles)
+listOfFiles= [f for f in os.listdir(root) if os.path.isfile(os.path.join(root, f)) and f.__contains__('.png')]
 runningIndex=0
 
 nextFrame = True
 currentFrame = True
 
+currentframepath='currentframe.png'
+drawLine1=False
+
+
+def draw(event, x, y, flags, param):
+    global ix, iy, drawing
+    ix, iy = x, y
+
+    if event == cv2.EVENT_RBUTTONDOWN:
+        if drawLine1:
+            line1.append([ix, iy])
+
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print('left btn clicked')
+
+
+    cframe = cv2.imread(currentframepath)
+    pts = np.array(line1, np.int32)
+    pts = pts.reshape((-1, 1, 2))
+    cv2.polylines(cframe, [pts], False, (0, 0, 255))
+    cv2.imshow(windowName, cframe)
+
+
+cv2.setMouseCallback(windowName,draw)
+
+
 while nextFrame:
 
 
-    # if os.path.isfile(listOfFiles[runningIndex]):
-    # if frame is not None:
-
     img = os.path.join(root, listOfFiles[runningIndex])
-
     frame = cv2.imread(img)
-    cv2.imwrite(currentframe, frame)
+    cv2.imwrite(currentframepath, frame)
 
 
     while currentFrame:
 
         # pts = np.array([100,150,170,180], np.int32)
-        # pts = pts.reshape((-1, 1, 2))
+        # pts = pts.reshape((-1, 1, 2))1
         # cv2.polylines(currentframe, [pts], True, (0, 255, 255))
+        cframe = cv2.imread(currentframepath)
 
-        cframe = cv2.imread(currentframe)
+
+
+        pts = np.array(line1, np.int32)
+        pts = pts.reshape((-1, 1, 2))
+        cv2.polylines(cframe, [pts], False, (0, 0, 255))
+
         cv2.imshow(windowName, cframe)
+
+
         ch = cv2.waitKey()
+        print(ch)
+
+
+        if ch==49:
+            drawLine1 = True
+            drawLine2 = False
+            drawCurve = False
+        #enter
+        if ch == 13:
+            drawLine1 = False
+            drawLine2 = False
+            drawCurve = False
 
         # Escape
-        if ch == 27:  # or cv2.getWindowProperty(windowName, 0) <= 0:
+        if ch == 27:
             cv2.destroyAllWindows()
             nextFrame=False
             currentFrame=False
@@ -64,8 +92,10 @@ while nextFrame:
             nextFrame = True
             currentFrame = False
 
+        #draw curve on space
+
 
     runningIndex = runningIndex + 1
 
-    if runningIndex > len(listOfFiles):
-       currentFrame = False
+    if runningIndex >= len(listOfFiles):
+        nextFrame = False
--- resize_images.py
+++ resize_images.py
@@ -1,6 +1,39 @@
 import os
 import cv2
 
+parkinglot=[]
+parkinglot.append([194, 122])
+parkinglot.append([203, 217])
+parkinglot.append([12, 251])
+parkinglot.append([8, 133])
+
+def get_crop_parmeter(parkinglot,outputFileName):
+    xmin=99999999
+    for x in parkinglot:
+        if x[0]< xmin:
+            xmin=x[0]
+    ymin = 99999999
+    for y in parkinglot:
+        if y[1] < ymin:
+            ymin = y[1]
+
+    xmax = 0
+    for x in parkinglot:
+        if x[0] > xmax:
+            xmax = x[0]
+    ymax = 0
+    for y in parkinglot:
+        if y[1] > ymax:
+            ymax = y[1]
+    x=xmin
+    y=ymin
+
+    h=ymax-ymin
+    w=xmax-xmin
+
+    crop_img = img[y: y + h, x: x + w]
+    cv2.imwrite(outputFileName, crop_img)
+
 root= 'data'
 
 listOfFiles= [os.path.join(root, f) for f in os.listdir(root) if os.path.isfile(os.path.join(root, f)) and f.__contains__('.JPG')]
@@ -14,7 +47,8 @@ for file in listOfFiles:
     nextFrame = True
 
     while nextFrame:
-        cv2.imshow('window', img)
+        resized_image = cv2.resize(img, (800, 600))
+        cv2.imshow('window', resized_image)
         ch = cv2.waitKey()
         if ch == 32:
             cv2.destroyAllWindows()
